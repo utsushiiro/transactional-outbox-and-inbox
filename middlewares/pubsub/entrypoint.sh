4e04138517e4d6ad84c10b735ee1c2c2767bb106
utsushiiro <ozeki.haru@gmail.com>
2025-01-02 00:24:12 +0900

    fix pubsub configs

--- middlewares/pubsub/entrypoint.sh
+++ middlewares/pubsub/entrypoint.sh
@@ -8,6 +8,8 @@ while ! nc -z localhost ${PUBSUB_EMULATOR_PORT}; do
   sleep 0.1
 done
 
+. venv/bin/activate
+
 python3 publisher.py $PUBSUB_PROJECT_ID create $PUBSUB_TOPIC_ID
 python3 subscriber.py $PUBSUB_PROJECT_ID create $PUBSUB_TOPIC_ID $PUBSUB_SUBSCRIPTION_ID
 
