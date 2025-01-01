025e7efcdaf46570b1642a0ce492b33672c4ea07
utsushiiro <ozeki.haru@gmail.com>
2025-01-01 22:30:49 +0900

    add middlewares

---
+++ middlewares/pubsub/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gcloud beta emulators pubsub start \
+  --project=$PUBSUB_PROJECT_ID \
+  --host-port=${PUBSUB_EMULATOR_HOST}:${PUBSUB_EMULATOR_PORT} --quiet &
+
+while ! nc -z localhost ${PUBSUB_EMULATOR_PORT}; do
+  sleep 0.1
+done
+
+python3 publisher.py $PUBSUB_PROJECT_ID create $PUBSUB_TOPIC_ID
+python3 subscriber.py $PUBSUB_PROJECT_ID create $PUBSUB_TOPIC_ID $PUBSUB_SUBSCRIPTION_ID
+
+fg %1
